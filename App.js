85be6fcbce921a00cd290bbc9c2bc1907ea29c7c
Olia Rosiak <olia.rosiak@gmail.com>
2023-07-05 22:57:34 +0300

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test screen of the application</Text>
       <StatusBar style="auto" />
     </View>
   );
